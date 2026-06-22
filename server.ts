4aa8308a3ea97aff59da0f11c8007c015005d6b8
emanueljefte <emanueldinganijoao13@gmail.com>
2026-06-22 16:18:14 +0100

    EntrevistaIA vercel

--- server.ts
+++ server.ts
@@ -704,7 +704,7 @@ async function startServer() {
     });
     app.use(vite.middlewares);
   } else {
-    const distPath = path.join(process.cwd(), "dist");
+    const distPath = path.join(__dirname, "../dist");
     app.use(express.static(distPath));
     app.get("*all", (req, res) => {
       res.sendFile(path.join(distPath, "index.html"));
